bc7483464770b97f262a6eaf3953f85576bf951c
Vincent <vincent168e@gmail.com>
2023-12-25 07:59:30 -0500

    add 1st line to AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,6 @@
 import React, { createContext, useReducer } from 'react';
+
+// 5. The reducer - this is used to update the state, based on the action
+export const AppReducer = (state, action) => {
+    let new_expenses = [];
+}
