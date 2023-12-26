9ce7ce76d3bfefa4a49665b1a654b3ee84729c51
Vincent <vincent168e@gmail.com>
2023-12-26 00:40:25 -0500

    completed

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -64,11 +64,11 @@ export const AppReducer = (state, action) => {
 // 1. Sets the initial state when the app loads
 const initialState = {
     expenses: [
-        {id: "Shirt", name: "Shirt", quantity: 0, unitprice: 500},
-        {id: "Jeans", name: "Jeans", quantity: 0, unitprice: 300},
-        {id: "Dress", name: "Dress", quantity: 0, unitprice: 400},
-        {id: "Dinner set", name: "Dinner set", quantity: 0, unitprice: 600},
-        {id: "Bags", name: "Bags", quantity: 0, unitprice: 200},
+        { id: "Shirt", name: "Shirt", quantity: 0, unitprice: 500 },
+        { id: "Jeans", name: "Jeans", quantity: 0, unitprice: 300 },
+        { id: "Dress", name: "Dress", quantity: 0, unitprice: 400 },
+        { id: "Dinner set", name: "Dinner set", quantity: 0, unitprice: 600 },
+        { id: "Bags", name: "Bags", quantity: 0, unitprice: 200 },
     ],
     Location: '£'
 };
